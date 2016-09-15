0a03c2e99536defc0884eea323d43e666411606c
ursula <frickeursu@icloud.com>
2016-09-15 17:19:54 +1200

    adds mouseenter and mouseleave events to div 2

--- events.js
+++ events.js
@@ -1,5 +1,5 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
@@ -7,9 +7,12 @@ function start () {
   // The first example is done for you. This will change the background colour of the first div
   // when you mouse over it.
   one()
-    
+  two()
+
   // Your turn! Create a new function called `two`, then call it from here.
 }
+
+
 
 function one () {
   // First, we have to find the element:
@@ -22,8 +25,13 @@ function one () {
   one.addEventListener('mouseleave', makeWhite)
 }
 
-// CREATE FUNCTION two HERE
+function two () {
+  var two = document.getElementById('two')
 
+  two.addEventListener('mouseenter', makeGreen)
+
+  two.addEventListener('mouseleave', makeWhite)
+}
 // CREATE FUNCTION three HERE
 
 // CREATE FUNCTION four HERE
@@ -35,4 +43,8 @@ function makeBlue (evt) {
 
 function makeWhite (evt) {
   evt.target.style.backgroundColor = 'white'
+}
+
+function makeGreen (evt) {
+  evt.target.style.backgroundColor = 'green'
 }
